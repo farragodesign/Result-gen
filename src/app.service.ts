e03ae9c69b1e7b0fea3f8d61a2f21205d0f5d270
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2023-07-14 09:40:21 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,9 @@ export class AppService {
     if (fs.existsSync('/tmp/schema.gql')){
       fs.readFile('/tmp/schema.gql', 'utf8', function(err, ss){
         data =ss
+        if(err){
+          data = err
+        }
       });
     }
       else{ data = 'no exists';
