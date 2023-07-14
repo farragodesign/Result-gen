7016d521b160b55b793fd2e812853d1b1198d9ed
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2023-07-14 14:15:36 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,8 @@
 import { Injectable } from '@nestjs/common';
-const fs = require('fs');
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    var data : any = ''
-    if (fs.existsSync('/tmp/schema.gql')){
-      fs.readFile('/tmp/schema.gql', 'utf8', function(err, ss){
-        data =ss
-        if(err){
-          data = err
-        }
-      });
-    }
-      else{ data = 'no exists';
-  
-}
-
-    return data;
+    return 'hello world';
   }
 }
